6a0547a79f54c1df058a3b8f6ad7120eb26ad5de
level3 <level3@admins-Mac-mini.local>
2025-05-21 11:56:25 +0200

    config lakefs container

--- init-db.sh
+++ init-db.sh
@@ -7,8 +7,23 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     
     CREATE USER dagster WITH PASSWORD 'dagster_password';
     CREATE DATABASE dagster OWNER dagster;
+
+    CREATE USER lakefs_user WITH PASSWORD 'lakefs_password';
+    CREATE DATABASE lakefs OWNER lakefs_user;
     
     \c dagster;
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dagster;
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO dagster;
+
+    \c mlflow;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO mlflow_user;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO mlflow_user;
+
+    \c lakefs;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO lakefs_user;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO lakefs_user;
+
+    GRANT ALL PRIVILEGES ON DATABASE mlflow TO mlflow_user;
+    GRANT ALL PRIVILEGES ON DATABASE dagster TO dagster;
+    GRANT ALL PRIVILEGES ON DATABASE lakefs TO lakefs_user;
 EOSQL
